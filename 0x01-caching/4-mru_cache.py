29d9fa0a4b591fc274a08bc10978ca8941c73844
Dev-wonderful <adeonderful20@gmail.com>
2023-10-26 04:58:02 +0100

    mru

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" LIFO Caching module
+""" MRU Caching module
 """
 from base_caching import BaseCaching
 
 
-class LRUCache(BaseCaching):
-    """ LRUCache defines:
-      - Last Recently Used algorithm for your caching system
+class MRUCache(BaseCaching):
+    """ MRUCache defines:
+      - Most Recently Used algorithm for your caching system
       - where your data are stored (in a dictionary)
     """
 
@@ -14,7 +14,7 @@ class LRUCache(BaseCaching):
         """ Initiliaze
         """
         super().__init__()
-        self.lru_queue = []
+        self.mru_queue = []
         self.queue = []
 
     def put(self, key, item):
@@ -23,29 +23,31 @@ class LRUCache(BaseCaching):
         if key and item:
             if key not in self.queue:
                 self.queue.append(key)
-                self.lru_queue.append(key)
+                
             self.cache_data[key] = item
             if len(self.cache_data.keys()) > self.MAX_ITEMS:
-                len_lru = len(self.lru_queue)
-                if len_lru > 0:
+                len_mru = len(self.mru_queue)
+                if len_mru > 0:
                     discard_list = [item for item in self.queue
-                                    if item not in self.lru_queue]
+                                    if item in self.mru_queue]
                     if len(discard_list) > 0:
-                        discard = discard_list.pop(0)
+                        discard = discard_list.pop(-1)
                     else:
-                        discard = self.lru_queue.pop(0)
+                        discard = self.mru_queue.pop(-1)
                     self.queue.remove(discard)
 
                 else:
-                    discard = self.queue.pop(0)
+                    discard = self.queue.pop(-1)
                 del self.cache_data[discard]
                 print("DISCARD: {}".format(discard))
+                self.mru_queue.append(key)
 
     def get(self, key):
         """ Get an item by key
         """
         if self.cache_data.get(key):
-            if key in self.lru_queue:
-                self.lru_queue.remove(key)
-            self.lru_queue.append(key)
+            if key in self.mru_queue:
+                self.mru_queue.remove(key)
+            self.mru_queue.append(key)
+            print(self.mru_queue)
         return self.cache_data.get(key, None)
