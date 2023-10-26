1b7a7d6b1a5fe47a3bc2028d8bd9d2c2f7bcfe9a
Dev-wonderful <adeonderful20@gmail.com>
2023-10-26 11:47:58 +0100

    MRU correction

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -2,7 +2,14 @@
 """ MRU Caching module
 """
 from base_caching import BaseCaching
-
+"""
+The difference between MRU and LIFO is that MRU updates the queue when
+you get or put an item unlike LIFO which only updates the queue when you put
+the item into the cache. If the item already exists in the queue it won't
+be moved to the back of the queue, unlike LIFO which would move the item
+to the back of the queue. MRU would only move the item to the back of the 
+queue when you access the item's data
+"""
 
 class MRUCache(BaseCaching):
     """ MRUCache defines:
@@ -15,32 +22,17 @@ class MRUCache(BaseCaching):
         """
         super().__init__()
         self.mru_queue = []
-        self.queue = []
 
     def put(self, key, item):
         """ Add an item in the cache
         """
         if key and item:
-            if key not in self.queue:
-                self.queue.append(key)
-
             self.cache_data[key] = item
             if len(self.cache_data.keys()) > self.MAX_ITEMS:
-                len_mru = len(self.mru_queue)
-                if len_mru > 0:
-                    discard_list = [item for item in self.queue
-                                    if item in self.mru_queue]
-                    if len(discard_list) > 0:
-                        discard = discard_list.pop(-1)
-                        self.mru_queue.remove(discard)
-                    else:
-                        discard = self.mru_queue.pop(-1)
-                    self.queue.remove(discard)
-
-                else:
-                    discard = self.queue.pop(-1)
+                discard = self.mru_queue.pop(-1)
                 del self.cache_data[discard]
                 print("DISCARD: {}".format(discard))
+            if key not in self.mru_queue:
                 self.mru_queue.append(key)
 
     def get(self, key):
