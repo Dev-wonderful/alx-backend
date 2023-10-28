8cd064ce6b5720e53bd151cab0ac4f45755e1a5e
Dev-wonderful <adeonderful20@gmail.com>
2023-10-28 03:27:48 +0100

    simple page

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -7,13 +7,11 @@ def index_range(page, page_size):
 
     Args:
         page (int): The page number whose index needs to be generated
-        page_isze (int): The data size of each page
+        page_size (int): The data size of each page
 
     Returns:
         tuple: a pair of values representing the start and end index
     """
-    assert page > 0
-    assert page_size > 0
 
     end = page * page_size
     start = end - page_size
---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,51 @@
+#!/usr/bin/pythons
+"""A simple pagination"""
+import csv
+import math
+from typing import List
+
+
+def index_range(page, page_size):
+    """Get the index range for a page
+
+    Args:
+        page (int): The page number whose index needs to be generated
+        page_isze (int): The data size of each page
+
+    Returns:
+        tuple: a pair of values representing the start and end index
+    """
+    assert page > 0
+    assert page_size > 0
+
+    end = page * page_size
+    start = end - page_size
+
+    return (start, end)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Get the page"""
+        assert page > 0
+        assert page_size > 0
+
+        index = index_range(page, page_size)
